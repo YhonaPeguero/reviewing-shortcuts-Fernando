337244f467fa2a67a3c8e5c20e09e66036ecdfed
Yhonatan Peguero <2017-0516@unad.edu.do>
2022-05-19 21:25:57 -0400

    first commit

---
+++ src/app/01-edicion-basica/08-deshacer-rehacer.ts
@@ -0,0 +1,20 @@
+/*
+    Objetivo:
+        Deshacer y rehacer el código
+§
+    Tips:
+        ⌘ Z
+        ⌘ ⇧ Z
+        Ctrl +Z
+        Ctrl + Shift + Z
+
+*/
+
+function holaMundo() {
+  return "Hola Mundo";
+}
+
+// Demo
+// function holaMundo() {
+//     return 'Saludos a todos!';
+// }
